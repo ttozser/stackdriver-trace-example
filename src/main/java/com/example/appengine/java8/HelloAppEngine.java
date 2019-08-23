cb942cf1b30561af7ceae0c430b97cb1cf34dc26
Tamás Tőzsér <tamas.tozser@aliz.ai>
2019-08-23 13:50:57 +0200

    add Stackdriver Trace

--- src/main/java/com/example/appengine/java8/HelloAppEngine.java
+++ src/main/java/com/example/appengine/java8/HelloAppEngine.java
@@ -17,36 +17,70 @@
 package com.example.appengine.java8;
 
 // [START example]
+
 import com.google.appengine.api.utils.SystemProperty;
 
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import io.opencensus.common.Scope;
+import io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
+import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
+import io.opencensus.trace.Tracer;
+import io.opencensus.trace.Tracing;
+import io.opencensus.trace.samplers.Samplers;
+
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
 @WebServlet(name = "HelloAppEngine", value = "/hello")
 public class HelloAppEngine extends HttpServlet {
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
-    Properties properties = System.getProperties();
-
-    response.setContentType("text/plain");
-    response.getWriter().println("Hello App Engine - Standard using "
-            + SystemProperty.version.get() + " Java "
-            + properties.get("java.specification.version"));
-  }
-
-  public static String getInfo() {
-    return "Version: " + System.getProperty("java.version")
-          + " OS: " + System.getProperty("os.name")
-          + " User: " + System.getProperty("user.name");
-  }
-
+	
+	private static final Tracer tracer = Tracing.getTracer();
+	
+	static {
+		try {
+			System.out.println("Init StackdriverTraceExporter");
+			StackdriverTraceExporter.createAndRegister(
+					StackdriverTraceConfiguration.builder()
+							.setProjectId("ttozser-da11e")
+							.build());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		Properties properties = System.getProperties();
+		
+		response.setContentType("text/plain");
+		response.getWriter().println("Hello App Engine - Standard using "
+				+ SystemProperty.version.get() + " Java "
+				+ properties.get("java.specification.version"));
+	}
+	
+	public static String getInfo() {
+		
+		try (Scope ignored = tracer.spanBuilder("MyChildWorkSpan").setSampler(Samplers.alwaysSample()).startScopedSpan()) {
+			tracer.getCurrentSpan().addAnnotation("annotation example");
+			try {
+				Thread.sleep(100);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			System.out.println("annotation created");
+			return "Version: " + System.getProperty("java.version")
+					+ " OS: " + System.getProperty("os.name")
+					+ " User: " + System.getProperty("user.name")
+					+ " Span: " + tracer.getCurrentSpan();
+		}
+	}
+	
 }
 // [END example]
